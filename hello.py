1af1243cb7450bdbfa4985e5ec572cb6649ac739
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2013-06-16 16:02:09 +0100

    Hello python update: testing queries

--- hello.py
+++ hello.py
@@ -4,5 +4,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    return 'The index page...'
+    
+@app.route('/querytest/<query>')
+def hello(query):
+    return 'Query %s' % query
